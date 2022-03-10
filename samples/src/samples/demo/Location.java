39cce1b5d9356d2dc17cb3c710537f1bf0679c2c
izzahhayati <b032010069@student.utem.edu.my>
2022-03-10 21:32:28 +0800

    Location Commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,13 @@
+package samples.demo;
+
+public class Location {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
