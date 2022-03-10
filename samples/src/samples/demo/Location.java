3e24de986a690e0e93601d474c28e6394d12b519
izzahhayati <b032010069@student.utem.edu.my>
2022-03-10 21:33:38 +0800

    Location Commit 2

--- samples/src/samples/demo/Location.java
+++ samples/src/samples/demo/Location.java
@@ -2,6 +2,15 @@ package samples.demo;
 
 public class Location {
 	private String name;
+	private int noPhone;
+
+	public int getNoPhone() {
+		return noPhone;
+	}
+
+	public void setNoPhone(int noPhone) {
+		this.noPhone = noPhone;
+	}
 
 	public String getName() {
 		return name;
